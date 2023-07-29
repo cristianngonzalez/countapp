ed65092feadb3e2a254621c5d605a1b122b072e4
Cristian Gonzalez <cgonzales@pxsol.com>
2023-07-29 13:15:32 -0300

    Migracion y modelo de countries

--- database/migrations/2023_07_29_155756_create_countries_table.php
+++ database/migrations/2023_07_29_155756_create_countries_table.php
@@ -4,15 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration{
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::table('countries', function (Blueprint $table) {
+    public function up(): void{
+        
+        Schema::create('countries', function (Blueprint $table) {
             //
+            $table->increments('id');
+            $table->string('name' , 255);
         });
     }
 
