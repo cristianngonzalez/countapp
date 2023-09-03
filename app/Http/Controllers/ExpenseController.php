a7b5886284b563171888fbe7806d14001f9d70d8
Cristian Gonzalez <cgonzales@pxsol.com>
2023-09-03 18:54:32 -0300

    expenses routes

---
+++ app/Http/Controllers/ExpenseController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExpenseController extends Controller
+{
+    //
+    public function list(Request $request){
+        return "Hello from ExpenseController";
+    }
+
+    public function info(Request $request){
+        return "Hello from ExpenseController";
+    }
+
+    public function add(Request $request){
+        return "Hello from ExpenseController";
+    }
+
+    public function edit(Request $request){
+        return "Hello from ExpenseController";
+    }
+
+    public function delete(Request $request){
+        return "Hello from ExpenseController";
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+//Controllers
+use App\Http\Controllers\ExpenseController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    //Expense routes
+   
+
+});
+
+
+Route::get('/expense/list', [ExpenseController::class, 'list']);
+Route::get('/expense/info/{id}', [ExpenseController::class, 'info']);
+Route::post('/expense/add', [ExpenseController::class, 'add']);
+Route::post('/expense/edit/{id}', [ExpenseController::class, 'edit']);
+Route::delete('/expense/delete/{id}', [ExpenseController::class, 'delete']);
